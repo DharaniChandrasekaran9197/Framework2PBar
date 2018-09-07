21f5b8c02893cc121d666e66a56c782e2dd545a7
Dharani Chandrasekaran <m1047034@mindtree.com>
2018-09-07 09:30:38 +0530

    framework

---
+++ src/main/java/Exceptions/AlertMessageMismatchException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class AlertMessageMismatchException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+public AlertMessageMismatchException()
+{
+	System.out.println("Alert Message is not Matching");
+}
+}
